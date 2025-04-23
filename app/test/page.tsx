15c338210f3134cd3929c23c1e8462eb1d54ca5a
izzanrijal <theprotectorboy@gmail.com>
2025-04-23 13:43:50 +0800

    try fix building

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,13 @@
 import TestInterface from '@/components/TestInterface'
+import { Suspense } from 'react'
+import type { JSX } from 'react'
 
 export default function TestPage() {
   return (
     <main className="min-h-screen bg-background p-4">
-      <TestInterface />
+      <Suspense fallback={<div>Loading...</div>}>
+        <TestInterface />
+      </Suspense>
     </main>
   )
 }
